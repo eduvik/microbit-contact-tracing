6012abc7eb2a40690b46b0567c322089c07d3e6b
Victor Rajewski <askvictor@gmail.com>
2020-06-02 09:44:05 +1000

    Change how to get serial number New animation sequence

--- contact_infections.py
+++ contact_infections.py
@@ -2,19 +2,7 @@ from microbit import *
 import radio
 import time
 import os
-
-def get_serial_number(type=hex):
-    NRF_FICR_BASE = 0x10000000
-    DEVICEID_INDEX = 25 # deviceid[1]
-    @micropython.asm_thumb
-    def reg_read(r0):
-        ldr(r0, [r0, 0])
-    n = reg_read(NRF_FICR_BASE + (DEVICEID_INDEX*4))
-    return type(n if n > 0 else n & (2**32-1))
-
-radio.on()
-
-ID = get_serial_number()[2:]
+import machine
 
 # all times in milliseconds
 DELAY_BETWEEN_BROADCASTS = 10 * 1000
@@ -25,6 +13,14 @@ TIME_BETWEEN_DATA_SAVES = 5 * 60 * 1000  # save data file this often
 RSSI_THRESHOLD = -60
 
 INFECTED_FILENAME = "infected"
+
+ID = "".join([ hex(a)[2:] for a in machine.unique_id()[4:] ])
+
+ANIMATION = (
+             Image("00000:00000:00100:00000:00000"),
+             Image("00000:01110:01010:01110:00000"),
+             Image("11111:10001:10001:10001:11111"),
+            )
 
 DATA_FILENAME = ID + "-1.csv"
 # check if data file exists; create new name to prevent overwriting
@@ -50,6 +46,8 @@ try:
 except OSError:
     infected = 0
 
+radio.on()
+
 while True:
     #if both buttons are pressed, dump data to serial port.
     if button_a.is_pressed() and button_b.is_pressed():
@@ -70,7 +68,7 @@ while True:
     d = radio.receive_full()
     while d:
         received_infected = False
-        display.show([Image.SQUARE, Image.SQUARE_SMALL], delay=50, wait=True, clear=True)
+        display.show(reversed(ANIMATION), delay=50, wait=True, clear=True)
         msg, rssi, timestamp = d
         timestamp = int(timestamp/1000)  # timestamp from radio is in microseconds; convert to milliseconds
         received_id = str(msg[3:], 'utf8')  #strip first three chars off the front; these are not part of the message
@@ -98,7 +96,7 @@ while True:
     #send message
     sleep(DELAY_BETWEEN_BROADCASTS/2)
     radio.send(ID + "!" if infected else ID)  # append ! to ID if we're infected
-    display.show([Image.SQUARE_SMALL, Image.SQUARE], delay=50, wait=True, clear=True)
+    display.show(ANIMATION, delay=50, wait=True, clear=True)
     sleep(DELAY_BETWEEN_BROADCASTS/2)
 
     #save data to file
